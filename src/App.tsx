c42b75381c2a915ac52f4ccd0299bd42dfcbbde4
Taeyooooon <eric568@naver.com>
2023-04-02 22:54:12 +0900

    Add : connect with openai api

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+
+import { sendMessageToGPT } from './sendMessage';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    sendMessageToGPT(inputValue);
+    console.log(inputValue);
+  };
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>OPENAI api</div>
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          placeholder='write name'
+          value={inputValue}
+          onChange={onChange}
+        />
+      </form>
+      <span></span>
+    </>
   );
 }
 
